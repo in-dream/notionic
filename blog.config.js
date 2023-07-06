e5f7c4cb74aab40b57a14ff227a7c1df4f22095b
chensu <35832000+in-dream@users.noreply.github.com>
2023-07-06 11:45:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -10,7 +10,7 @@ const BLOG = {
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#46454575', // use hex value, don't forget '#'
+  darkBackground: '#fffefc', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Notionic in a folder
   since:  , // If leave this empty, current year will be used.
   postsPerPage: 12,
