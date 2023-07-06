8ef28230bffe25dc9aed8dfb3d79bee0e2de3b1e
chensu <35832000+in-dream@users.noreply.github.com>
2023-07-06 11:48:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -9,8 +9,8 @@ const BLOG = {
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#fffefc', // use hex value, don't forget '#'
+  lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#212936', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Notionic in a folder
   since:  , // If leave this empty, current year will be used.
   postsPerPage: 12,
